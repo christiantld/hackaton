8edaccdd8e9986baa17e5e0229310decec434c8e
Rafael Vieira <rafaelleveske@gmail.com>
2020-07-05 17:42:39 -0300

    Atualizando o PointsController

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import multer from "multer";
-import multerConfig from "./config/multer";
+import express from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-import PointsController from "./controllers/PointsController";
-import ServicesController from "./controllers/ServicesController";
-import SessionController from "./controllers/SessionController";
+import PointsController from './controllers/PointsController';
+import ServicesController from './controllers/ServicesController';
+import SessionController from './controllers/SessionController';
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -14,13 +14,13 @@ const servicesController = new ServicesController();
 const sessionController = new SessionController();
 
 // Create point
-routes.post("/points", upload.single("image"), pointsController.store);
+routes.post('/points', upload.single('image'), pointsController.store);
 
 // Sign In
-routes.post("/session", sessionController.store);
+routes.post('/session', sessionController.store);
 
-routes.get("/services", servicesController.index);
-routes.get("/points/:id", pointsController.show);
-routes.get("/points", pointsController.index);
+routes.get('/services', servicesController.index);
+routes.get('/points/:id', pointsController.show);
+routes.get('/points', pointsController.index);
 
 export default routes;
